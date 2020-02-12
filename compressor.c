8241985f827fd8fd8c59e4b07acff9ba5df207eb
Flavien Henrion <flavien.henrion@protonmail.com>
2020-02-12 20:33:14 +0100

    it works

--- compressor.c
+++ compressor.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 15:52:37 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/02/10 18:31:15 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/02/12 20:27:55 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 /*
 ** TODO :
-** - ajouter la longeur de la chaine DNA source dans le fichier compressé
 ** - vérifier l'entrée (ACTG)
 ** - permettre whitespace + trim
-** - décompression avec lecture de longueur pour prendre en compte le padding
 */
 
 /*
@@ -30,10 +28,9 @@
 
 # include <stdio.h>
 
-static void		compress(int fd, char *dna)
+static void		compress(int fd, char *dna, size_t size)
 {
 	size_t	i;
-	size_t	size = strlen(dna);
 	uint8_t	block;
 
 	printf("%lu\n", size);
@@ -43,34 +40,27 @@ static void		compress(int fd, char *dna)
 		block = 0b00000000;
 		for (i = 0; i < 8 && *dna; i += 2, dna++)
 		{
-			if (*dna == 'C')
-				block |= 0b01 << i;
+			if (*dna == 'A')
+				block |= (0b00 << i);
+			else if (*dna == 'C')
+				block |= (0b01 << i);
 			else if (*dna == 'G')
-				block |= 0b10 << i;
-			else
-				block |= 0b11 << i;
+				block |= (0b10 << i);
+			else if (*dna == 'T')
+				block |= (0b11 << i);
 		}
 		write(fd, &block, 1);
 	}
 }
 
-static size_t	get_size(char *block)
-{
-	size_t	i;
-	size_t	size = 0;
-
-	for (i = 0; i < sizeof(size_t); i++)
-		size |= block[i] << (i * BITS_PER_BYTE);
-	return (size);
-}
-
-static void		decompress_block(int fd, char block, int size)
+static void		decompress_block(int fd, char block, size_t size)
 {
 	size_t	i;
 	char	dna;
 
 	for (i = 0; i < size; i += 2)
 	{
+		dna = 0;
 		dna = (0b11 & (block >> i));
 		printf("%c", dna);
 		if (dna == 0b00)
@@ -85,23 +75,18 @@ static void		decompress_block(int fd, char block, int size)
 	}
 }
 
-static void		decompress(int fd, char *block)
+static void		decompress(int fd, char *block, size_t size)
 {
-	size_t	i;
-	//size_t	size = get_size(block);
-	size_t size = 192000;
 	int		pad = size % 4;
 
 	printf("%lu\n", size);
-	block += sizeof(size_t);
-	size -= sizeof(size_t);
 	while ((size -= 4))
 	{
 		decompress_block(fd, *block, 8);
 		block++;
 	}
-	//pad = pad ? pad * 2 : 8;
-	//decompress_block(fd, *block, pad);
+	pad = pad ? pad * 2 : 8;
+	decompress_block(fd, *block, pad);
 }
 
 static char		*read_file(int fd, size_t offset)
@@ -125,6 +110,7 @@ static char		*read_file(int fd, size_t offset)
 
 int		main(int ac, char **av)
 {
+	size_t	size;
 	int		fd_read;
 	int		fd_write;
 	char	*dna;
@@ -139,7 +125,8 @@ int		main(int ac, char **av)
 			return (1);
 		if (!(dna = read_file(fd_read, 0)))
 			return (1);
-		compress(fd_write, dna);
+		size = strlen(dna);
+		compress(fd_write, dna, size);
 	}
 	else if (!strcmp("DNA.dnz", av[1]))
 	{
@@ -147,9 +134,10 @@ int		main(int ac, char **av)
 			return (1);
 		if ((fd_write = open("DNA.dna", O_WRONLY | O_CREAT, 0644)) < 0)
 			return (1);
+		read(fd_read, &size, sizeof(size_t));
 		if (!(dna = read_file(fd_read, 0)))
 			return (1);
-		decompress(fd_write, dna);
+		decompress(fd_write, dna, size);
 	}
 	return (0);
 }
